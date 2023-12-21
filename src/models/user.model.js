85430f2a924087649cfbab00b4fdfcc1316dc72d
sourav-mahmud <souravmahmud32@gmail.com>
2023-12-21 15:50:49 +0600

    fix register controller all bugs

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,6 +1,6 @@
 import mongoose, {Schema} from "mongoose";
-import { jwt } from "jsonwebtoken";
-import bcrypt from bcrypt;
+import jwt  from "jsonwebtoken";
+import bcrypt from "bcrypt";
 
 const userSchema = new Schema(
     {
@@ -25,7 +25,7 @@ const userSchema = new Schema(
             trim: true,
             index: true
         },
-        avater: {
+        avatar: {
             type: String, // cloudinary url
             required: true
         },
